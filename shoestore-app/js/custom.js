a631603d0db4c6ea2bbbd8a220e68c7c0d65b24b
Raymond Thompson <ray_thomp@hushmail.com>
2018-01-08 13:26:30 +0000

    laptop-change

--- shoestore-app/js/custom.js
+++ shoestore-app/js/custom.js
@@ -1,33 +1,47 @@
 
 $(document).ready(function() {
 
-        $('button#side-bar-toggle-btn').on('click', function(){
+	var $mainmenu = $('button#side-bar-toggle-btn');
+	
+	$mainmenu.on('click', function(event){
 
-           		show_menu();
+		event.preventDefault();
 
-			});
+		show_menu();
 
-            function show_menu(){
+});
+	
 
-			$('#content #left_bar').toggle(500, function(){
+function show_menu(){
 
-				$(this).animate({
-                    'display' : 'block',
-                    left :'0.5em' });
+	// create menu variables
+	var slideoutMenu = $('#content #left_bar');
+	var slideoutMenuWidth = $('#content #left_bar').width();
 
-			});
+	// toggle open class
+	slideoutMenu.toggleClass("open");
+
+	// slide menu
+	if (slideoutMenu.hasClass("open")) {
+		slideoutMenu.animate({
+			left: "0px"
+		});	
+	} else {
+		slideoutMenu.animate({
+			left: -slideoutMenuWidth
+		}, 500);	
+	}
+}
 
 
+//menu
+$('button#menu-toggle-btn').on('click', function(event){
 
-			}
+	event.preventDefault();
 
+	$('nav ul#mobile-nav').slideToggle(500).css('display' , 'block');
 
-
-        $('button#menu-toggle-btn').on('click', function(){
-
-            $('nav ul#mobile-nav').slideToggle(500).css('display' , 'block');
-
-        });
+});
 
 
 
