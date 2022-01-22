96dd856df3ba09545f8309d11f187794d8b5dee2
raythompsonwebdev <ray_thomp@hushmail.com>
2022-01-22 18:17:33 +0000

    refactored accordion using map function

--- src/components/FindShoesAccord.js
+++ src/components/FindShoesAccord.js
@@ -1,101 +1,29 @@
 import React from "react";
 import { Accordion } from "react-bootstrap";
+import accordion from "./accordion.json";
 
 function FindShoesAccord() {
+  // eslint-disable-next-line no-console
+  // console.log(accordion);
+
   return (
     <Accordion defaultActiveKey={["0"]} alwaysOpen>
       <nav id="acc_nav" aria-label="secondary">
         <div id="accContainer">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Mens Shoes</Accordion.Header>
-            <Accordion.Body>
-              <div className="panel">
-                <ul>
-                  <li>Skate Shoes</li>
-                  <li>Boat Shoes</li>
-                  <li>Fashion Sneakers</li>
-                  <li>Formal &amp; Tuxedo</li>
-                  <li>Shoes</li>
-                  <li>Loafers &amp; Slip-ons</li>
-                  <li>Outdoor</li>
-                  <li>Sandals</li>
-                  <li>Slippers</li>
-                  <li>Work &amp; Safety</li>
-                  <li>More...</li>
-                </ul>
-              </div>
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Womens Shoes</Accordion.Header>
-            <Accordion.Body>
-              <div className="panel">
-                <ul>
-                  <li>Skate Shoes</li>
-                  <li>Boat Shoes</li>
-                  <li>Fashion Sneakers</li>
-                  <li>Formal &amp; Dress</li>
-                  <li>Shoes</li>
-                  <li>Loafers &amp; Slip-ons</li>
-                  <li>Outdoor</li>
-                  <li>Sandals</li>
-                  <li>Slippers</li>
-                  <li>Work &amp; Safety</li>
-                  <li>More...</li>
-                </ul>
-              </div>
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="2">
-            <Accordion.Header>Kids Shoes</Accordion.Header>
-            <Accordion.Body>
-              <div className="panel">
-                <ul>
-                  <li>Skate Shoes</li>
-                  <li>Boat Shoes</li>
-                  <li>Fashion Sneakers</li>
-                  <li>Formal &amp; Dress</li>
-                  <li>Shoes</li>
-                  <li>Loafers &amp; Slip-ons</li>
-                  <li>Outdoor</li>
-                  <li>Sandals</li>
-                  <li>Slippers</li>
-                  <li>Work &amp; Safety</li>
-                  <li>More...</li>
-                </ul>
-              </div>
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="3">
-            <Accordion.Header>Accessories</Accordion.Header>
-            <Accordion.Body>
-              <div className="panel">
-                <ul>
-                  <li>Skate Shoes</li>
-                  <li>Fashion Sneakers</li>
-                  <li>Formal &amp; Dress</li>
-                  <li>Shoes</li>
-                  <li>Outdoor</li>
-                  <li>Sandals</li>
-                  <li>Slippers</li>
-                  <li>More...</li>
-                </ul>
-              </div>
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="4">
-            <Accordion.Header>Accessories</Accordion.Header>
-            <Accordion.Body>
-              <div className="panel">
-                <ul>
-                  <li>Bags</li>
-                  <li>Belts</li>
-                  <li>Scarfs</li>
-                  <li>Hats</li>
-                </ul>
-              </div>
-            </Accordion.Body>
-          </Accordion.Item>
+          {accordion.map((element) => (
+            <Accordion.Item eventKey={element.id} key={element.id}>
+              <Accordion.Header>{element.title}</Accordion.Header>
+              <Accordion.Body>
+                <div className="panel">
+                  <ul>
+                    {element.items.map((ele) => (
+                      <li>{ele}</li>
+                    ))}
+                  </ul>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
         </div>
       </nav>
     </Accordion>
