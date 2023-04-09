83ef49c21a64323f352b8ca82ab155e48964d2bd
raythompsonwebdev <ray_thomp@hushmail.com>
2023-04-09 21:41:55 +0100

    add mungodb & typescript

--- pages/api/likeproduct.js
+++ pages/api/likeproduct.js
@@ -4,7 +4,6 @@ export async function likeproduct(req, res) {
     res.status(405).send({ message: "Only POST requests allowed" });
     return;
   }
-
   const body = req.body;
   res.status(200).send(body);
 }
